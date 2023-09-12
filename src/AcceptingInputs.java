e7f3075a2d7ba5752d35617bc10093de3256fff1
David Arhin <davidarhin2005@gmail.com>
2023-09-12 23:03:06 +0000

    printing out inputs

--- src/AcceptingInputs.java
+++ src/AcceptingInputs.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AcceptingInputs {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        System.out.println();
+    String input = scanner.nextLine();
+        System.out.println(input);
     }
 }
