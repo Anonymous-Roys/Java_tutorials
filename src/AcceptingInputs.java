0a55b2557ef087f9e85fe3328798437d7a690158
David Arhin <davidarhin2005@gmail.com>
2023-09-12 23:02:20 +0000

    accepting inputs

--- src/AcceptingInputs.java
+++ src/AcceptingInputs.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class AcceptingInputs {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+        System.out.println();
     }
 }
