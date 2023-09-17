5fab8aaf5fcdf7d800bb15aded320fc2b22af831
David Arhin <davidarhin2005@gmail.com>
2023-09-17 23:05:58 +0000

    variables in java

--- src/VariablesInJava.java
+++ src/VariablesInJava.java
@@ -1,5 +1,11 @@
 public class VariablesInJava {
     public static void main(String[] args) {
         //variables in Java
+        int a = 39;
+        int b ,c = 4;
+        String names = "Kwabena";
+        System.out.println(names);
+        System.out.println(a);
+        System.out.println(b + " = "+ c);
     }
 }
